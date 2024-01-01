f4f46d00a32042b019d67375e7852bd5565ac420
jayeshjadhav <jayeshjadhav684@gmail.com>
2024-01-01 12:23:35 +0530

    dashboard changes

--- src/app/views/dashboard/shared/StatCards.jsx
+++ src/app/views/dashboard/shared/StatCards.jsx
@@ -1,5 +1,7 @@
 import { Box, Card, Grid, Icon, IconButton, styled, Tooltip } from '@mui/material';
 import { Small } from 'app/components/Typography';
+import { useEffect, useState } from 'react'; 
+const axios = require('axios');
 
 const StyledCard = styled(Card)(({ theme }) => ({
   display: 'flex',
@@ -31,31 +33,54 @@ const StatCards = () => {
   const cardList = [
     { name: 'New Leads', amount: 3050, icon: 'group' },
     { name: 'This week Sales', amount: '$80,500', icon: 'attach_money' },
-    { name: 'Inventory Status', amount: '8.5% Stock Surplus', icon: 'store' },
-    { name: 'Orders to deliver', amount: '305 Orders', icon: 'shopping_cart' },
+    // { name: 'Inventory Status', amount: '8.5% Stock Surplus', icon: 'store' },
+    // { name: 'Orders to deliver', amount: '305 Orders', icon: 'shopping_cart' },
   ];
+  const [logCount, setLogCount] = useState(0);
+
+  useEffect(() => {
+      let data = JSON.stringify({});
+
+      let config = {
+        method: 'post',
+        url: 'http://127.0.0.1:4330/api/getDeviceLogCount',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+
+      axios.request(config)
+      .then((response) => {
+        setLogCount(response.data.totalSize)
+        // console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },[])
 
   return (
     <Grid container spacing={3} sx={{ mb: '24px' }}>
-      {cardList.map((item, index) => (
-        <Grid item xs={12} md={6} key={index}>
+      {/* {cardList.map((item, index) => ( */}
+        <Grid item xs={12} md={6} >
           <StyledCard elevation={6}>
             <ContentBox>
-              <Icon className="icon">{item.icon}</Icon>
+              <Icon className="icon">group</Icon>
               <Box ml="12px">
-                <Small>{item.name}</Small>
-                <Heading>{item.amount}</Heading>
+                <Small>Log Count</Small>
+                <Heading>{logCount}</Heading>
               </Box>
             </ContentBox>
 
             <Tooltip title="View Details" placement="top">
-              <IconButton>
+              {/* <IconButton>
                 <Icon>arrow_right_alt</Icon>
-              </IconButton>
+              </IconButton> */}
             </Tooltip>
           </StyledCard>
         </Grid>
-      ))}
+      {/* ))} */}
     </Grid>
   );
 };
