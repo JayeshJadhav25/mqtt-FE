ad7b11ae5ab58e856c0a8e611e5a222ba5fdb13a
Akashkumar <akash.gupta@slashrtc.com>
2025-05-10 15:57:16 +0530

    1. minimized ui build

--- src/app/contexts/SettingsContext.js
+++ src/app/contexts/SettingsContext.js
@@ -1,7 +1,5 @@
 import React, { createContext, useState } from 'react'
-
 import { merge } from 'lodash'
-
 import { MatxLayoutSettings } from 'app/components/MatxLayout/settings'
 
 const SettingsContext = createContext({
